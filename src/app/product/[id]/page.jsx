1bbc0868a18966c4eb4f1d41d95b839e4f622d37
Pedro Miguel <pmig.magalhaes@gmail.com>
2025-10-16 17:25:57 +0200

    Add stock info in ProductCards

--- src/app/product/[id]/page.jsx
+++ src/app/product/[id]/page.jsx
@@ -1,10 +1,10 @@
-'use client';
+"use client";
 
-import { useParams } from 'next/navigation';
-import { useSelector, useDispatch } from 'react-redux';
-import { addToCart } from '../../../features/cart/cartSlice';
-import { useState, useEffect } from 'react';
-import Skeleton from '@components/Skeleton';
+import { useParams } from "next/navigation";
+import { useSelector, useDispatch } from "react-redux";
+import { addToCart } from "../../../features/cart/cartSlice";
+import { useState, useEffect } from "react";
+import Skeleton from "@components/Skeleton";
 
 export default function ProductDetails() {
   const { id } = useParams();
@@ -21,36 +21,59 @@ export default function ProductDetails() {
   const convert = (price) => (price * rates[current]).toFixed(2);
   const getSymbol = () => {
     switch (current) {
-      case 'USD': return '$';
-      case 'GBP': return '£';
-      default: return '€';
+      case "USD":
+        return "$";
+      case "GBP":
+        return "£";
+      default:
+        return "€";
     }
   };
 
   if (!mounted || !product) {
-  return (
-    <div style={{ padding: '1rem' }}>
-      <Skeleton width="300px" height="300px" style={{ marginBottom: '1rem' }} />
-      <Skeleton width="50%" height="1.5rem" style={{ marginBottom: '0.5rem' }} />
-      <Skeleton width="80%" height="1rem" style={{ marginBottom: '0.5rem' }} />
-      <Skeleton width="100px" height="2rem" borderRadius="8px" style={{ marginTop: '0.5rem' }} />
-    </div>
-  );
-}
+    return (
+      <div style={{ padding: "1rem" }}>
+        <Skeleton
+          width="300px"
+          height="300px"
+          style={{ marginBottom: "1rem" }}
+        />
+        <Skeleton
+          width="50%"
+          height="1.5rem"
+          style={{ marginBottom: "0.5rem" }}
+        />
+        <Skeleton
+          width="80%"
+          height="1rem"
+          style={{ marginBottom: "0.5rem" }}
+        />
+        <Skeleton
+          width="100px"
+          height="2rem"
+          borderRadius="8px"
+          style={{ marginTop: "0.5rem" }}
+        />
+      </div>
+    );
+  }
 
   return (
-    <div style={{ padding: '1rem' }}>
+    <div style={{ padding: "1rem" }}>
+      <button>Back</button>
       <h1>{product.title}</h1>
-      <img src={product.image} alt={product.title} width={300} style={{ objectFit: 'contain' }} />
+      <img
+        src={product.image}
+        alt={product.title}
+        width={150}
+        style={{ objectFit: "contain" }}
+      />
       <p>{product.description}</p>
-      <p>{getSymbol()}{convert(product.price)}</p>
-      <button
-        onClick={() => dispatch(addToCart(product))}
-        style={{ marginTop: '0.5rem', padding: '0.5rem 1rem', cursor: 'pointer' }}
-      >
-        Add to Cart
-      </button>
+      <p>
+        {getSymbol()}
+        {convert(product.price)}
+      </p>
+      <button onClick={() => dispatch(addToCart(product))}>Add to Cart</button>
     </div>
   );
 }
-
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,12 +1,11 @@
-'use client';
+"use client";
 
-import styles from './styles/ProductCard.module.css';
-import Link from 'next/link';
-import { useDispatch, useSelector } from 'react-redux';
-import { addToCart } from '../features/cart/cartSlice';
-import { useState, useEffect } from 'react';
-import Skeleton from './Skeleton';
-
+import styles from "./styles/ProductCard.module.css";
+import Link from "next/link";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart } from "../features/cart/cartSlice";
+import { useState, useEffect } from "react";
+import Skeleton from "./Skeleton";
 
 export default function ProductCard({ product }) {
   const dispatch = useDispatch();
@@ -18,22 +17,42 @@ export default function ProductCard({ product }) {
   const convert = (price) => (price * rates[current]).toFixed(2);
   const getSymbol = () => {
     switch (current) {
-      case 'USD': return '$';
-      case 'GBP': return '£';
-      default: return '€';
+      case "USD":
+        return "$";
+      case "GBP":
+        return "£";
+      default:
+        return "€";
     }
   };
 
+  const randomStock = () => {
+    let randNum = Math.random();
+    if (randNum > 0.2) return "In stock";
+    return "Sold Out";
+  };
+
+  const stockStatus = randomStock();
+  const stockColor = stockStatus === "In stock" ? "#1a140ece " : "#630d0df3";
+
   if (!mounted) {
-  return (
-    <div className={styles.groupOfSkeletons}>
-      <Skeleton width="150px" height="150px" style={{ margin: '0 auto' }} />
-      <Skeleton width="80%" height="1rem" style={{ margin: '0.5rem auto' }} />
-      <Skeleton width="50px" height="1rem" style={{ margin: '0.5rem auto' }} />
-      <Skeleton width="80px" height="2rem" style={{ margin: '0.5rem auto' }} />
-    </div>
-  );
-}
+    return (
+      <div className={styles.groupOfSkeletons}>
+        <Skeleton width="150px" height="150px" style={{ margin: "0 auto" }} />
+        <Skeleton width="80%" height="1rem" style={{ margin: "0.5rem auto" }} />
+        <Skeleton
+          width="50px"
+          height="1rem"
+          style={{ margin: "0.5rem auto" }}
+        />
+        <Skeleton
+          width="80px"
+          height="2rem"
+          style={{ margin: "0.5rem auto" }}
+        />
+      </div>
+    );
+  }
 
   return (
     <div className={styles.productCard}>
@@ -41,13 +60,23 @@ export default function ProductCard({ product }) {
         <img src={product.image} alt={product.title} />
         <h3>{product.title}</h3>
       </Link>
-      <p>{getSymbol()}{convert(product.price)}</p>
-      <button
-        onClick={() => dispatch(addToCart(product))}
-        style={{ marginTop: '0.5rem', padding: '0.5rem 1rem', cursor: 'pointer' }}
+      <p>
+        {getSymbol()}
+        {convert(product.price)}
+      </p>
+      <p
+        className={styles.stockInfo}
+        style={{
+          color: stockColor,
+        }}
       >
-        Add to Cart
-      </button>
+        {stockStatus}
+      </p>
+      {stockStatus === "In stock" && (
+        <button onClick={() => dispatch(addToCart(product))}>
+          Add to Cart
+        </button>
+      )}
     </div>
   );
 }
