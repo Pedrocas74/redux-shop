559f58643727349b1ce8188c3e4f3ab0d8317572
Pedro Miguel <pmig.magalhaes@gmail.com>
2025-10-23 14:14:49 +0200

    Add details and summaries to each product page

--- src/app/product/[id]/page.jsx
+++ src/app/product/[id]/page.jsx
@@ -4,10 +4,9 @@ import styles from "./ProductDetails.module.css";
 import { useParams } from "next/navigation";
 import { useSelector, useDispatch } from "react-redux";
 import { addToCart } from "../../../features/cart/cartSlice";
-import { useState, useEffect } from "react"
-import Link from "next/link";
+import { useState, useEffect } from "react";
 import Skeleton from "@components/Skeleton";
-import Navbar from "@components/Navbar";
+import Image from "next/image";
 
 export default function ProductDetails() {
   const { id } = useParams();
@@ -35,27 +34,23 @@ export default function ProductDetails() {
 
   if (!mounted || !product) {
     return (
-      <div style={{ padding: "1em" }}>
+      <div
+        style={{
+          height: "85vh",
+          padding: "1em",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-around",
+        }}
+      >
+        <Skeleton width="70%" height="1.5rem" style={{ marginLeft: "2em" }} />
+        <Skeleton width="25%" height="1rem" style={{ marginLeft: "2em" }} />
+        <Skeleton width="80%" height="50vh" style={{ margin: "0 auto" }} />
         <Skeleton
-          width="30%"
-          height="1.5rem"
-          style={{ marginBottom: "0.5rem" }}
-        />
-        <Skeleton
-          width="80%"
-          height="1rem"
-          style={{ marginBottom: "0.5rem" }}
-        />
-        <Skeleton
-          width="300px"
-          height="300px"
-          style={{ marginBottom: "1rem" }}
-        />
-        <Skeleton
-          width="100px"
+          width="120px"
           height="2rem"
           borderRadius="8px"
-          style={{ marginTop: "0.5rem" }}
+          style={{ margin: "0 auto" }}
         />
       </div>
     );
@@ -64,13 +59,12 @@ export default function ProductDetails() {
   return (
     <div className={styles.pageContainer}>
       <section className={styles.detailsContainer}>
-        {/* <button>Back</button> */}
-        <span>HomePage/category</span>
+        <span>Home/Category</span>
         <h1>{product.title}</h1>
-        <p className={styles.price}>
+        <h2 className={styles.price}>
           {getSymbol()}
           {convert(product.price)}
-        </p>
+        </h2>
         <figure>
           <img
             src={product.image}
@@ -78,7 +72,7 @@ export default function ProductDetails() {
             style={{ objectFit: "contain" }}
           />
         </figure>
-        
+
         {product.sizes && (
           <div className={styles.sizeSelector}>
             {product.sizes.map((size) => (
@@ -89,16 +83,107 @@ export default function ProductDetails() {
           </div>
         )}
 
-        <Link href='/'><span>See the product description</span></Link>
+        <div className={styles.buttonAddContainer}>
+          {product.stock === "In stock" && (
+            <button
+              className={styles.buttonAdd}
+              onClick={() => dispatch(addToCart(product))}
+            >
+              Add to Cart
+            </button>
+          )}
+          {product.stock === "Sold out" && (
+            <button
+              className={styles.buttonAdd}
+              onClick={() => dispatch(addToCart(product))}
+              disabled
+              style={{ cursor: "not-allowed" }}
+            >
+              Out of stock
+            </button>
+          )}
+        </div>
 
-        {product.stock === "In stock" && (
-          <button
-            className={styles.buttonAdd}
-            onClick={() => dispatch(addToCart(product))}
-          >
-            Add to Cart
-          </button>
-        )}
+        {/* DESCRIPTION OF THE PRODUCT */}
+        <details>
+          <summary className={styles.instructions}>Description</summary>
+          <p className={styles.productDescription}>{product.description}</p>
+        </details>
+
+        {/* DELIVERY AND RETURN RULES */}
+        <details>
+          <summary className={styles.instructions}>
+            Free delivery and returns
+          </summary>
+          <ul className={styles.careList}>
+            <li>✓ 30 Day Returns </li>
+            <li>✓ Free Worldwide Shipping </li>
+            <li>✓ Shipped within 48 hours</li>
+          </ul>
+        </details>
+
+        {/* WASHING INSTRUCTIONS */}
+        <details>
+          <summary className={styles.instructions}>Care</summary>
+          <h3 className={styles.instructionsTitle}>Washing Instructions</h3>
+          <ul className={styles.careList}>
+            <li>
+              <span className={styles.iconWrapper}>
+                <Image
+                  src="/images/icons/instructions/not-bleach.png"
+                  alt="Do not bleach"
+                  fill
+                  priority
+                />
+              </span>
+              Do not bleach
+            </li>
+            <li>
+              <span className={styles.iconWrapper}>
+                <Image
+                  src="/images/icons/instructions/tumble-dry.png"
+                  alt="Do not tumble dry"
+                  fill
+                  priority
+                />
+              </span>
+              Do not tumble dry
+            </li>
+            <li>
+              <span className={styles.iconWrapper}>
+                <Image
+                  src="/images/icons/instructions/do-not-dry.png"
+                  alt="Do not dry clean"
+                  fill
+                  priority
+                />
+              </span>
+              Do not dry clean
+            </li>
+            <li>
+              <span className={styles.iconWrapper}>
+                <Image
+                  src="/images/icons/instructions/ironing.png"
+                  alt="Iron on the lowest setting"
+                  fill
+                  priority
+                />
+              </span>
+              Iron on the lowest setting
+            </li>
+            <li>
+              <span className={styles.iconWrapper}>
+                <Image
+                  src="/images/icons/instructions/wash-30.png"
+                  alt="Wash at low temperature"
+                  fill
+                  priority
+                />
+              </span>
+              Wash at low temperature and on delicate cycle
+            </li>
+          </ul>
+        </details>
       </section>
     </div>
   );
